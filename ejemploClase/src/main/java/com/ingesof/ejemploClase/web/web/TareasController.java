7cfc7ac5f40840d02f60d4bf00c064404666600a
Katheryn Guasca <katheryng2017@gmail.com>
2024-10-09 11:57:24 +0000

    Agregando metodo solicitar tarea

--- ejemploClase/src/main/java/com/ingesof/ejemploClase/web/web/TareasController.java
+++ ejemploClase/src/main/java/com/ingesof/ejemploClase/web/web/TareasController.java
@@ -1,8 +1,12 @@
 package com.ingesof.ejemploClase.web.web;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ingesof.ejemploClase.web.accesoDatos.TareaRepository;
+import com.ingesof.ejemploClase.web.modelo.Tarea;
 
 @RestController
 public class TareasController {
@@ -10,5 +14,11 @@ public class TareasController {
 
     public TareasController(TareaRepository tareas){ 
         this.tareas= tareas; 
+        
+    //muestre todas las tareas
+    @GetMapping("/tareas")
+    public List<Tarea> mostrarTareas(){
+        return tareas.findAll(); 
+    
     }
 }
